852c2384bba8e157c78ce95028d622c5d64db8d2
Scuolaagraria-dotcom <stefania.parasiliti@marconifpbz.it>
2026-05-26 00:47:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const URL="https://teachablemachine.withgoogle.com/models/ImxAX5Cz0/";
+const URL="https://teachablemachine.withgoogle.com/models/LIY6s69qI/";
 
 let model, webcam, labelContainer, maxPredictions;
 
